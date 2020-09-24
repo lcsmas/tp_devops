e6f0ee3c4bf44f71d51fabc5b50251de3d5c87d5
lucas <lucas_mas@msn.com>
2020-09-24 16:54:53 +0200

    stuf

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,3 +72,8 @@ tasks.withType<Test> {
 tasks.withType<Wrapper> {
     gradleVersion = "5.6"
 }
+
+task<Exec>("build-image") {
+    workingDir("docker")
+    commandLine("docker", "build", "." , "-t", "app")
+}
